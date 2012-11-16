3be7e6597ac60b7bd8a02115758dd0790383b55f
jsamos <jsamos@gmail.com>
2012-11-16 14:10:31 -0800

    add namespacing and extend from AbstractPlugin

--- yuicompressor.php
+++ yuicompressor.php
@@ -1,11 +1,14 @@
 <?php
 
-class YUICompressor
+namespace YUI\YUICompressor; 
+use Zend\Mvc\Controller\Plugin\AbstractPlugin;
+ 
+class YUICompressor  extends AbstractPlugin
 {
     
     // absolute path to YUI jar file.
-    private static $JAR_PATH;
-    private static $TEMP_FILES_DIR;
+    private $JAR_PATH;
+    private $TEMP_FILES_DIR;
     private $options = array('type' => 'js',
                              'linebreak' => false,
                              'verbose' => false,
@@ -51,55 +54,53 @@ class YUICompressor
         
         // read the input
         foreach ($this->files as $file) {
-    		$this->string .= file_get_contents($file) or die("Cannot read from uploaded file");        
+            $this->string .= file_get_contents($file) or die("Cannot read from uploaded file");        
         }
-    	
+        
         // create single file from all input
         $input_hash = sha1($this->string);
         $file = $this->TEMP_FILES_DIR . '/' . $input_hash . '.txt';
         $fh = fopen($file, 'w') or die("Can't create new file");
         fwrite($fh, $this->string);
         fclose($fh);
-    	
-    	// start with basic command
+        
+        // start with basic command
         $cmd = "java -Xmx32m -jar " . escapeshellarg($this->JAR_PATH) . ' ' . escapeshellarg($file) . " --charset UTF-8";
     
         // set the file type
-    	$cmd .= " --type " . (strtolower($this->options['type']) == "css" ? "css" : "js");
-    	
-    	// and add options as needed
-    	if ($this->options['linebreak'] && intval($this->options['linebreak']) > 0) {
+        $cmd .= " --type " . (strtolower($this->options['type']) == "css" ? "css" : "js");
+        
+        // and add options as needed
+        if ($this->options['linebreak'] && intval($this->options['linebreak']) > 0) {
             $cmd .= ' --line-break ' . intval($this->options['linebreak']);
-    	}
+        }
 
-    	if ($this->options['verbose']) {
-    	   $cmd .= " -v";
+        if ($this->options['verbose']) {
+           $cmd .= " -v";
         }
             
-		if ($this->options['nomunge']) {
-			$cmd .= ' --nomunge';
-		}
-		
-		if ($this->options['semi']) {
-			$cmd .= ' --preserve-semi';
-		}
-		
-		if ($this->options['nooptimize']) {
-			$cmd .= ' --disable-optimizations';
-		}
+        if ($this->options['nomunge']) {
+            $cmd .= ' --nomunge';
+        }
+        
+        if ($this->options['semi']) {
+            $cmd .= ' --preserve-semi';
+        }
+        
+        if ($this->options['nooptimize']) {
+            $cmd .= ' --disable-optimizations';
+        }
     
         // execute the command
-    	exec($cmd . ' 2>&1', $raw_output);
-    	
-    	// add line breaks to show errors in an intelligible manner
+        exec($cmd . ' 2>&1', $raw_output);
+        
+        // add line breaks to show errors in an intelligible manner
         $flattened_output = implode("\n", $raw_output);
-    	
-    	// clean up (remove temp file)
-    	unlink($file);
-    	
-    	// return compressed output
-    	return $flattened_output;
+        
+        // clean up (remove temp file)
+        unlink($file);
+        
+        // return compressed output
+        return $flattened_output;
     }
 }
-
-?>
